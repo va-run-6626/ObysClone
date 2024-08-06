4a9d7ceb6288013b20efacb7087019e512f7c2bd
va-run-6626 <vbhutada143@gmail.com>
2024-08-06 17:04:44 +0530

    Made Loader

---
+++ script.js
@@ -0,0 +1,48 @@
+
+
+
+
+
+
+
+const tl = gsap.timeline()
+tl.from(".line h1",{
+    y:150,
+    opacity:0,
+    stagger: 0.25,
+    duration : 0.6,
+    delay : 0.5
+})
+tl.from("#line1Part1",{
+    opacity:0,
+    onStart : () =>{
+        const h5Timer = document.querySelector("#line1Part1 h5");
+        var grow = 0
+        const counter = setInterval(()=>{
+            if(grow <= 100){
+                h5Timer.innerHTML = grow++;
+            }else{
+                clearInterval(counter)
+            }
+        },35)
+    }
+})
+tl.to(".line h2",{
+    opacity: 1,
+    animationName : "animateNow"
+})
+tl.to("#loader",{
+    opacity : 0,
+    duration: 0.2,
+    delay : 4
+})
+tl.from("#page1",{
+    y:1600,
+    opacity:0,
+    delay:0.2,
+    ease: Power4,
+    duration:0.5
+})
+tl.to("#loader",{
+    display:"none"
+})
